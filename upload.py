cc2f4531bcb77168da135ecb2a7080ca39849269
Debbie Cohen <dcohen1999@gmail.com>
2021-12-28 17:36:42 -0500

    fixing .heic display

--- upload.py
+++ upload.py
@@ -1,11 +1,13 @@
 import tornado.httpserver, tornado.ioloop, tornado.options, tornado.web, os.path, random, string
-
+from PIL import Image
+# import pyheif
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", IndexHandler),
-            (r"/upload", UploadHandler)
+            (r"/upload", UploadHandler),
+            (r"/convert", ConvertHandler)
         ]
         tornado.web.Application.__init__(self, handlers)
 
@@ -17,11 +19,23 @@ class UploadHandler(tornado.web.RequestHandler):
     def post(self):
         file1 = self.request.files['file1'][0]
         original_fname = file1['filename']
-
-        output_file = open("uploads/" + original_fname, 'wb')
-        output_file.write(file1['body'])
+        #convert here
+        with open("uploads/" + original_fname, 'wb') as output_file:
+            output_file.write(file1['body'])
+        
 
         self.finish("file " + original_fname + " is uploaded")
+
+class ConvertHandler(tornado.web.RequestHandler):
+    def post(self):
+        print(self.request)
+        # file1 = self.request.files['file1'][0]
+        # img = convertFile(file1)
+        # self.write(img)
+        # self.write(file1['body'])
+        self.finish()
+
+
 
 settings = {
 # 'template_path': 'templates',
@@ -29,13 +43,25 @@ settings = {
 "xsrf_cookies": False
 
 }
+
 application = tornado.web.Application([
    (r"/", IndexHandler),
-            (r"/upload", UploadHandler)
+            (r"/upload", UploadHandler),
+            (r"/convert", ConvertHandler)
 ], debug=True,**settings)
 
 
-
+def convertFile(file):
+    print(file)
+    # file = pyheif.read("IMG_7424.HEIC")
+    # image = Image.frombytes(
+    #     heif_file.mode, 
+    #     heif_file.size, 
+    #     heif_file.data,
+    #     "raw",
+    #     heif_file.mode,
+    #     heif_file.stride,
+    # )
 print ("Server started.")
 if __name__ == "__main__":
     application.listen(8888)
